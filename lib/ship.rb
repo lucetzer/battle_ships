606ef92eb7f5148d55ca40f0db20873c1dc2f2a4
Aaron <aarongkendall@gmail.com>
2015-09-07 12:05:40 +0100

    direction and size of ship

--- lib/ship.rb
+++ lib/ship.rb
@@ -3,4 +3,12 @@ class Ship
   def place_ship
   end
 
+  def size(type)
+    @size = type
+  end
+
+  def direction(orientation)
+    @direction = orientation
+  end
+
 end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -6,4 +6,12 @@ describe Ship do
     is_expected.to respond_to :place_ship
   end
 
+  it 'responds to size with 1 argument' do
+    is_expected.to respond_to(:size).with(1).argument
+  end
+
+  it 'responds to direction with 1 argument' do
+    is_expected.to respond_to(:direction).with(1).argument
+  end
+
 end
