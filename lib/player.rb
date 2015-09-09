3425bdb48f411d90ec12cd9fc68d1839988b6942
Aaron <aarongkendall@gmail.com>
2015-09-09 12:19:04 +0100

    player can place ship

--- lib/player.rb
+++ lib/player.rb
@@ -2,6 +2,13 @@ require_relative 'board'
 
 class Player
 
+  attr_reader :board
+
+  def initialize
+    @board = Board.new
+  end
+
   def place(ship)
+    board.receive_ship(ship)
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,4 +4,9 @@ describe Player do
   it 'responds to place method with argument of ship' do
     expect(subject).to respond_to(:place).with(1).argument
   end
+
+  it 'places ships onto the board' do
+    ship = Ship.new "A1"
+    expect(subject.place(ship)).to eq(subject.board.ship_array)
+  end
 end
